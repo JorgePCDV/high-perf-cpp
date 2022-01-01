8e1b48fe8e05c6d5ff257a57a088b41dd738c77e
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2022-01-01 17:22:40 +0000

    vanilla option example

--- cpp-course/cpp-course.cpp
+++ cpp-course/cpp-course.cpp
@@ -2,6 +2,9 @@
 //
 
 #include <iostream>
+#include "vanilla_option.h"
+
+
 #include "Log.h"
 #include "Pointers.h"
 #include "References.h"
@@ -10,15 +13,40 @@
 #include "Entity.h"
 #include "PlayerEntity.h"
 
+void VanillaOptionExample();
+void VanillaOptionExample() {
+    VanillaOption option;  // Create the vanilla option
+
+  // Calculate the call and put prices
+    double call = option.calc_call_price();
+    double put = option.calc_put_price();
+
+    // Output the option parameters
+    std::cout << "Strike, K: " << option.getK() << std::endl;
+    std::cout << "Risk-free rate, r: " << option.getr() << std::endl;
+    std::cout << "Time to maturity, T: " << option.getT() << std::endl;
+    std::cout << "Spot price, S: " << option.getS() << std::endl;
+    std::cout << "Volatility of asset, sigma: " << option.getsigma() << std::endl;
+
+    // Output the option prices
+    std::cout << "Call Price: " << call << std::endl;
+    std::cout << "Put Price: " << put << std::endl;
+}
+
 int main()
 {
+    VanillaOptionExample();
+    
+    
+    
+    
     // Log("init");
     //Pointers();
     //References();
     //PlayerExample();
     //ClassStructure();
     //Constructors();
-    Inheritance();
+    //Inheritance();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
