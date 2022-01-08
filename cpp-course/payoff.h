6e89dffe647d56ae925e891c3a4ef72fbf59d01b
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2022-01-08 16:13:08 +0000

    PayOffCall and PayOffPut in payoff header

--- cpp-course/payoff.h
+++ cpp-course/payoff.h
@@ -4,14 +4,38 @@
 #include <algorithm> // Needed for std::max comparison function
 
 class PayOff {
-	public:
-		PayOff(); // Default (no parameter) constructor
-		virtual ~PayOff() {}; // Virtual destructor
+public:
+	PayOff(); // Default (no parameter) constructor
+	virtual ~PayOff() {}; // Virtual destructor
 
-		// Overloaded () operator
-		// Turns PayOff into abstract function object
-		// = 0 makes it pure virtual
-		virtual double operator() (const double S) const = 0;
+	// Overloaded () operator
+	// Turns PayOff into abstract function object
+	// = 0 makes it pure virtual
+	virtual double operator() (const double S) const = 0;
+};
+
+class PayOffCall : public PayOff {
+private:
+	double K; // Strike price
+	
+public:
+	PayOffCall(const double K_);
+	virtual ~PayOffCall() {};
+
+	// Virtual function is now over-ridden (not pure-virtual anymore)
+	virtual double operator() (const double S) const;
+};
+
+class PayOffPut : public PayOff {
+private:
+	double K; // Strike price
+
+public:
+	PayOffPut(const double K_);
+	virtual ~PayOffPut() {};
+
+	// Virtual function is now over-ridden (not pure-virtual anymore)
+	virtual double operator() (const double S) const;
 };
 
 #endif
