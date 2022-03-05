d5b702d5d220f931441c103b37ed5e8c2721da60
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2022-03-05 15:14:18 +0000

    Monte Carlo Put Price

--- cpp-course/european_monte_carlo.h
+++ cpp-course/european_monte_carlo.h
@@ -2,3 +2,6 @@ double gaussian_box_muller();
 double monte_carlo_call_price(const int& num_sims, const double& S,
 							  const double& K, const double& r,
 							  const double& v, const double& T);
+double monte_carlo_put_price(const int& num_sims, const double& S,
+							  const double& K, const double& r,
+							  const double& v, const double& T);
