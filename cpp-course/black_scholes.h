5bb0b365d0758fd082fcdb9e583934df18f05e61
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2022-03-08 20:45:36 +0000

    Put greeks

--- cpp-course/black_scholes.h
+++ cpp-course/black_scholes.h
@@ -9,3 +9,10 @@ double call_gamma(const double S, const double K, const double r, const double v
 double call_vega(const double S, const double K, const double r, const double v, const double T);
 double call_theta(const double S, const double K, const double r, const double v, const double T);
 double call_rho(const double S, const double K, const double r, const double v, const double T);
+
+double put_price(const double S, const double K, const double r, const double v, const double T);
+double put_delta(const double S, const double K, const double r, const double v, const double T);
+double put_gamma(const double S, const double K, const double r, const double v, const double T);
+double put_vega(const double S, const double K, const double r, const double v, const double T);
+double put_theta(const double S, const double K, const double r, const double v, const double T);
+double put_rho(const double S, const double K, const double r, const double v, const double T);
