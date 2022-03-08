fa9331b85bf25df4e04d66763f37c563da4122bf
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2022-03-08 20:41:29 +0000

    Call greeks

---
+++ cpp-course/black_scholes.h
@@ -0,0 +1,11 @@
+#pragma once
+
+double norm_pdf(const double x);
+double norm_cdf(const double x);
+double d_j(const int j, const double S, const double K, const double r, const double v, const double T);
+double call_price(const double S, const double K, const double r, const double v, const double T);
+double call_delta(const double S, const double K, const double r, const double v, const double T);
+double call_gamma(const double S, const double K, const double r, const double v, const double T);
+double call_vega(const double S, const double K, const double r, const double v, const double T);
+double call_theta(const double S, const double K, const double r, const double v, const double T);
+double call_rho(const double S, const double K, const double r, const double v, const double T);
