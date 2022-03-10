804abdfbe9ba235a4bc7257e3a4eac50c7fe8d98
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2022-03-10 21:27:54 +0000

    Grees with finite difference method

--- cpp-course/black_scholes.cpp
+++ cpp-course/black_scholes.cpp
@@ -96,6 +96,18 @@ double put_rho(const double S, const double K, const double r, const double v, c
     return -T * K * exp(-r * T) * norm_cdf(-d_j(2, S, K, r, v, T));
 }
 
+// This uses the forward difference approximation to calculate the Delta of a call option
+double call_delta_fdm(const double S, const double K, const double r, const double v, const double T, const double delta_S)
+{
+    return (call_price(S + delta_S, K, r, v, T) - call_price(S, K, r, v, T)) / delta_S;
+}
+
+// This uses the centred difference approximation to calculate the Gamma of a call option
+double call_gamma_fdm(const double S, const double K, const double r, const double v, const double T, const double delta_S)
+{
+    return (call_price(S + delta_S, K, r, v, T) - 2 * call_price(S, K, r, v, T) + call_price(S - delta_S, K, r, v, T)) / (delta_S * delta_S);
+}
+
 void GreeksAnalyticExample()
 {
     double S = 100.0;  // Option price
--- cpp-course/black_scholes.h
+++ cpp-course/black_scholes.h
@@ -17,4 +17,7 @@ double put_vega(const double S, const double K, const double r, const double v, 
 double put_theta(const double S, const double K, const double r, const double v, const double T);
 double put_rho(const double S, const double K, const double r, const double v, const double T);
 
+double call_delta_fdm(const double S, const double K, const double r, const double v, const double T, const double delta_S);
+double call_gamma_fdm(const double S, const double K, const double r, const double v, const double T, const double delta_S);
+
 void GreeksAnalyticExample();
